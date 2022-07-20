0c26d8568802da83d28b78a81ac7af2be749aef3
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2022-07-20 17:45:57 +0300

    Exercise 10.2: setting up the ESLint

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! It is working!</Text>
+      <Text>Open up App.js to start working on your app! Does this work any more?</Text>
       <StatusBar style="auto" />
     </View>
   );
